961d984eee49221d71a239fd3045b342f0105da7
Ian Ynda-Hummel <ianynda@gmail.com>
2014-09-28 18:01:59 -0400

    Actually make Crashlytics work

--- bin/CrashlyticsKeys.sh
+++ bin/CrashlyticsKeys.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -f crashlytics_api_key ]; then
+if [ -f Highball/crashlytics_api_key ]; then
     export CRASHLYTICS_API_KEY="$(cat Highball/crashlytics_api_key)"
 fi
 
-if [ -f crashlytics_app_key ]; then
+if [ -f Highball/crashlytics_app_key ]; then
     export CRASHLYTICS_APP_KEY="$(cat Highball/crashlytics_app_key)"
 fi
