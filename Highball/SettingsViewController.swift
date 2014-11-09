cb217342c85ae7471db0aea3da7fd35911f08e26
Ian Ynda-Hummel <ianynda@gmail.com>
2014-11-08 22:06:43 -0500

    Create passcode

--- Highball/SettingsViewController.swift
+++ Highball/SettingsViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class SettingsViewController : UITableViewController {
+    private enum PasscodeRow: Int {
+        case Set
+        case UseTouch
+    }
     let settingsCellIdentifier = "settingsCellIdentifier"
 
     override func viewDidLoad() {
@@ -30,15 +34,48 @@ class SettingsViewController : UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if VENTouchLock.canUseTouchID() {
+            return 2
+        }
         return 1
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCellWithIdentifier(settingsCellIdentifier, forIndexPath: indexPath) as UITableViewCell
 
-        cell.textLabel.text = "Passcode"
-        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        switch PasscodeRow(rawValue: indexPath.row)! {
+        case .Set:
+            cell.textLabel.text = "Passcode"
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        case .UseTouch:
+            cell.textLabel.text = "Use Touch ID"
+            if VENTouchLock.shouldUseTouchID() {
+                cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+            } else {
+                cell.accessoryType = UITableViewCellAccessoryType.None
+            }
+        }
 
         return cell
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        switch PasscodeRow(rawValue: indexPath.row)! {
+        case .Set:
+            let viewController = VENTouchLockCreatePasscodeViewController()
+            if let navigationController = self.navigationController {
+                viewController.willFinishWithResult = { finished in
+                    navigationController.popViewControllerAnimated(true); return
+                }
+                navigationController.pushViewController(viewController, animated: true)
+            }
+        case .UseTouch:
+            if VENTouchLock.shouldUseTouchID() {
+                VENTouchLock.setShouldUseTouchID(false)
+            } else {
+                VENTouchLock.setShouldUseTouchID(true)
+            }
+            tableView.reloadData()
+        }
     }
 }
